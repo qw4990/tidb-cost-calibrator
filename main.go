ee07f45ae7443247837358e52bb137e9c7c1b5a3
qw4990 <zhangyuanjia@pingcap.com>
2022-08-22 11:28:08 +0800

    fixup

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/spf13/cobra"
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "optimizer-tester",
+		Short: "TiDB Optimizer Tester",
+	}
+)
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	cobra.OnInitialize()
+}
+
+func main() {
+
+}
