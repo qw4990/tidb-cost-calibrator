9312c0e06476e4ab64ee96fb46645b8d4d55a597
qw4990 <zhangyuanjia@pingcap.com>
2022-08-23 19:34:34 +0800

    fixup

--- costeval/synthetic.go
+++ costeval/synthetic.go
@@ -10,30 +10,51 @@ func genSYNQueries(ins utils.Instance, db string, n int) utils.Queries {
 	ins.MustExec(fmt.Sprintf("use %v", db))
 
 	var qs utils.Queries
+	qs = append(qs, genSYNScans(ins, n)...)
 	qs = append(qs, genSYNAgg(ins, n)...)
 	return qs
 }
 
 func genSYNScans(ins utils.Instance, n int) utils.Queries {
-	// TODO:
-	return nil
+	ps := []pattern{
+		// TiKV Table Scan
+		{`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a from t where %v`,
+			"a", 1000, "TableScan"},
+		//{`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a, c from t where %v`,
+		//	"a", 1000, "WideTableScan"},
+		//{`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a from t where %v order by a desc`,
+		//	"a", 1000, "DescTableScan"},
+		//
+		//// TiKV Index Scan
+		//{`select /*+ use_index(t, b), read_from_storage(tikv[t]) */ b from t where %v`,
+		//	"b", 1000, "IndexScan"},
+		//{`select /*+ use_index(t, bc), read_from_storage(tikv[t]) */ b, c from t where %v`,
+		//	"b", 1000, "DescIndexScan"},
+		//{`select /*+ use_index(t, b), read_from_storage(tikv[t]) */ b from t where %v order by b desc`,
+		//	"b", 1000, "DescIndexScan"},
+		//
+		//// MPP Scan
+		//{`SELECT /*+ read_from_storage(tiflash[t]) */ a FROM t WHERE %v`,
+		//	"a", 1000, "MPPScan"},
+	}
+	return gen4Patterns(ins, ps, n)
 }
 
 func genSYNAgg(ins utils.Instance, n int) utils.Queries {
 	ps := []pattern{
-		// TiKV SA
+		// TiKV StreamAgg
 		{`select /*+ read_from_storage(tikv[t]), stream_agg(), use_index(t, b) */ sum(a) from t where %v`,
 			"b", 2000000, "StreamAgg1"},
 		{`select /*+ read_from_storage(tikv[t]), stream_agg(), use_index(t, b) */ sum(a) from t where %v`,
 			"b", 2000000, "StreamAgg2"},
 
-		// TiKV HA
+		// TiKV HashAgg
 		{`select /*+ read_from_storage(tikv[t]), use_index(t, b), hash_agg() */ count(1) from t where %v`,
 			"b", 10000000, "HashAgg1"}, // agg without group-by
 		{`select /*+ read_from_storage(tikv[t]), use_index(t, b), hash_agg() */ count(b), b from t where %v group by b`,
 			"b", 3000000, "HashAgg2"}, // agg with group-by
 
-		// MPP HA
+		// MPP HashAgg
 		{`select /*+ read_from_storage(tiflash[t]), mpp_tidb_agg() */ count(a) from t where %v`,
 			"b", 3000000, "MPPTiDBAgg1"}, // mpp tidb agg without group-by
 		{`select /*+ read_from_storage(tiflash[t]), mpp_tidb_agg() */ count(a), b from t where %v group by b`,
