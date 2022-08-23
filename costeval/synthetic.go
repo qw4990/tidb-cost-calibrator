c86a7404fa0fc2194a7df59f32f4d11a5051a6c3
qw4990 <zhangyuanjia@pingcap.com>
2022-08-23 20:06:18 +0800

    fixup

--- costeval/synthetic.go
+++ costeval/synthetic.go
@@ -19,7 +19,7 @@ func genSYNScans(ins utils.Instance, n int) utils.Queries {
 	ps := []pattern{
 		// TiKV Table Scan
 		{`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a from t where %v`,
-			"a", 20000000, "TableScan"},
+			"a", 10000000, "TableScan"},
 		//{`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a, c from t where %v`,
 		//	"a", 1000, "WideTableScan"},
 		//{`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a from t where %v order by a desc`,
@@ -44,25 +44,25 @@ func genSYNAgg(ins utils.Instance, n int) utils.Queries {
 	ps := []pattern{
 		// TiKV StreamAgg
 		{`select /*+ read_from_storage(tikv[t]), stream_agg(), use_index(t, b) */ sum(a) from t where %v`,
-			"b", 20000000, "StreamAgg1"},
-		{`select /*+ read_from_storage(tikv[t]), stream_agg(), use_index(t, b) */ sum(a) from t where %v`,
-			"b", 20000000, "StreamAgg2"},
+			"b", 8000000, "StreamAgg1"},
+		{`select /*+ read_from_storage(tikv[t]), stream_agg(), use_index(t, b) */ sum(a) from t where %v group by b`,
+			"b", 5000000, "StreamAgg2"},
 
 		// TiKV HashAgg
 		{`select /*+ read_from_storage(tikv[t]), use_index(t, b), hash_agg() */ count(1) from t where %v`,
 			"b", 10000000, "HashAgg1"}, // agg without group-by
 		{`select /*+ read_from_storage(tikv[t]), use_index(t, b), hash_agg() */ count(b), b from t where %v group by b`,
-			"b", 2000000, "HashAgg2"}, // agg with group-by
+			"b", 3000000, "HashAgg2"}, // agg with group-by
 
 		// MPP HashAgg
 		{`select /*+ read_from_storage(tiflash[t]), mpp_tidb_agg() */ count(a) from t where %v`,
-			"b", 12000000, "MPPTiDBAgg1"}, // mpp tidb agg without group-by
+			"b", 20000000, "MPPTiDBAgg1"}, // mpp tidb agg without group-by
 		{`select /*+ read_from_storage(tiflash[t]), mpp_tidb_agg() */ count(a), b from t where %v group by b`,
 			"b", 2000000, "MPPTiDBAgg2"}, // mpp tidb agg with group-by
 		{`select /*+ read_from_storage(tiflash[t]), mpp_1phase_agg() */ count(a), b from t where %v group by b`,
-			"b", 15000000, "MPP1PhaseAgg"},
+			"b", 8000000, "MPP1PhaseAgg"},
 		{`select /*+ read_from_storage(tiflash[t]), mpp_2phase_agg() */ count(a), b from t where %v group by b`,
-			"b", 15000000, "MPP2PhaseAgg"},
+			"b", 8000000, "MPP2PhaseAgg"},
 	}
 	return gen4Patterns(ins, ps, n)
 }
