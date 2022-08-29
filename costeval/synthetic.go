447358634f908b230c15f1e1a0816009ee31d3ac
qw4990 <zhangyuanjia@pingcap.com>
2022-08-29 20:29:43 +0800

    fixup

--- costeval/synthetic.go
+++ costeval/synthetic.go
@@ -1,103 +1,140 @@
 package costeval
 
 import (
-	"fmt"
-
 	"github.com/qw4990/tidb-cost-calibrator/utils"
 )
 
-func genSYNQueries(ins utils.Instance, db string, n int) utils.Queries {
-	ins.MustExec(fmt.Sprintf("use %v", db))
-
+func genSYNQueries(n int) utils.Queries {
 	var qs utils.Queries
-	qs = append(qs, genSYNScans(ins, n)...)
-	qs = append(qs, genSYNAgg(ins, n)...)
-	qs = append(qs, genSYNJoin(ins, n)...)
-	qs = append(qs, genSYNSort(ins, n)...)
+	qs = append(qs, genSYNScans(n)...)
+	qs = append(qs, genSYNAgg(n)...)
+	qs = append(qs, genSYNJoin(n)...)
+	qs = append(qs, genSYNSort(n)...)
 	return qs
 }
 
-func genSYNSort(ins utils.Instance, n int) utils.Queries {
-	ps := []pattern{
-		{`select /*+ use_index(t, bc), read_from_storage(tikv[t]) */ b from t where %v order by c`,
-			"b", 2500000, "Sort"},
+func genSYNSort(n int) utils.Queries {
+	return gen4Templates([]template{
+		{
+			`select /*+ use_index(t, bc), read_from_storage(tikv[t]) */ b from t where %v order by c`,
+			[]tempitem{{"t", "b", 0, 2500000}},
+			"Sort",
+		},
 		{`select /*+ use_index(t, bc), read_from_storage(tikv[t]) */ b from t where %v order by c limit 1000`,
-			"b", 5000000, "TopN"},
-	}
-	return gen4Patterns(ins, ps, n)
+			[]tempitem{{"t", "b", 0, 5000000}},
+			"TopN",
+		},
+	}, n)
 }
 
-func genSYNScans(ins utils.Instance, n int) utils.Queries {
-	ps := []pattern{
+func genSYNScans(n int) utils.Queries {
+	return gen4Templates([]template{
 		// TiKV Table Scan
-		{`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a from t where %v`,
-			"a", 10000000, "TableScan"},
-		{`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a, c from t where %v`,
-			"a", 3000000, "WideTableScan"},
-		{`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a from t where %v order by a desc`,
-			"a", 6000000, "DescTableScan"},
+		{
+			`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a from t where %v`,
+			[]tempitem{{"t", "a", 0, 10000000}},
+			"TableScan"},
+		{
+			`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a, c from t where %v`,
+			[]tempitem{{"t", "a", 0, 3000000}},
+			"WideTableScan"},
+		{
+			`select /*+ use_index(t, primary), read_from_storage(tikv[t]) */ a from t where %v order by a desc`,
+			[]tempitem{{"t", "a", 0, 6000000}},
+			"DescTableScan"},
 
 		// TiKV Index Scan
-		{`select /*+ use_index(t, b), read_from_storage(tikv[t]) */ b from t where %v`,
-			"b", 10000000, "IndexScan"},
-		{`select /*+ use_index(t, bc), read_from_storage(tikv[t]) */ b, c from t where %v`,
-			"b", 3000000, "DescIndexScan"},
-		{`select /*+ use_index(t, b), read_from_storage(tikv[t]) */ b from t where %v order by b desc`,
-			"b", 6000000, "DescIndexScan"},
+		{
+			`select /*+ use_index(t, b), read_from_storage(tikv[t]) */ b from t where %v`,
+			[]tempitem{{"t", "b", 0, 10000000}},
+			"IndexScan"},
+		{
+			`select /*+ use_index(t, bc), read_from_storage(tikv[t]) */ b, c from t where %v`,
+			[]tempitem{{"t", "b", 0, 3000000}},
+			"DescIndexScan"},
+		{
+			`select /*+ use_index(t, b), read_from_storage(tikv[t]) */ b from t where %v order by b desc`,
+			[]tempitem{{"t", "b", 0, 6000000}},
+			"DescIndexScan"},
 
 		// TiKV Index Lookup
-		{`select /*+ use_index(t, b), read_from_storage(tikv[t]) */ b, d from t where %v`,
-			"b", 250000, "IndexLookup"},
+		{
+			`select /*+ use_index(t, b), read_from_storage(tikv[t]) */ b, d from t where %v`,
+			[]tempitem{{"t", "b", 0, 250000}},
+			"IndexLookup"},
 
 		// MPP Scan
-		{`select /*+ read_from_storage(tiflash[t]) */ a from t where %v`,
-			"a", 20000000, "MPPScan"},
-	}
-	return gen4Patterns(ins, ps, n)
+		{
+			`select /*+ read_from_storage(tiflash[t]) */ a from t where %v`,
+			[]tempitem{{"t", "a", 0, 20000000}},
+			"MPPScan"},
+	}, n)
 }
 
-func genSYNAgg(ins utils.Instance, n int) utils.Queries {
-	ps := []pattern{
+func genSYNAgg(n int) utils.Queries {
+	return gen4Templates([]template{
 		// TiKV StreamAgg
-		{`select /*+ read_from_storage(tikv[t]), stream_agg(), use_index(t, b) */ sum(a) from t where %v`,
-			"b", 8000000, "StreamAgg1"},
-		{`select /*+ read_from_storage(tikv[t]), stream_agg(), use_index(t, b) */ sum(a) from t where %v group by b`,
-			"b", 5000000, "StreamAgg2"},
+		{
+			`select /*+ read_from_storage(tikv[t]), stream_agg(), use_index(t, b) */ sum(a) from t where %v`,
+			[]tempitem{{"t", "b", 0, 8000000}},
+			"StreamAgg1"},
+		{
+			`select /*+ read_from_storage(tikv[t]), stream_agg(), use_index(t, b) */ sum(a) from t where %v group by b`,
+			[]tempitem{{"t", "b", 0, 5000000}},
+			"StreamAgg2"},
 
 		// TiKV HashAgg
-		{`select /*+ read_from_storage(tikv[t]), use_index(t, b), hash_agg() */ count(1) from t where %v`,
-			"b", 10000000, "HashAgg1"}, // agg without group-by
-		{`select /*+ read_from_storage(tikv[t]), use_index(t, b), hash_agg() */ count(b), b from t where %v group by b`,
-			"b", 2200000, "HashAgg2"}, // agg with group-by
+		{
+			`select /*+ read_from_storage(tikv[t]), use_index(t, b), hash_agg() */ count(1) from t where %v`,
+			[]tempitem{{"t", "b", 0, 10000000}},
+			"HashAgg1"}, // agg without group-by
+		{
+			`select /*+ read_from_storage(tikv[t]), use_index(t, b), hash_agg() */ count(b), b from t where %v group by b`,
+			[]tempitem{{"t", "b", 0, 2200000}},
+			"HashAgg2"}, // agg with group-by
 
 		// MPP HashAgg
-		{`select /*+ read_from_storage(tiflash[t]), mpp_tidb_agg() */ count(a) from t where %v`,
-			"b", 20000000, "MPPTiDBAgg1"}, // mpp tidb agg without group-by
-		{`select /*+ read_from_storage(tiflash[t]), mpp_tidb_agg() */ count(a), b from t where %v group by b`,
-			"b", 2000000, "MPPTiDBAgg2"}, // mpp tidb agg with group-by
-		{`select /*+ read_from_storage(tiflash[t]), mpp_1phase_agg() */ count(a), b from t where %v group by b`,
-			"b", 8000000, "MPP1PhaseAgg"},
-		{`select /*+ read_from_storage(tiflash[t]), mpp_2phase_agg() */ count(a), b from t where %v group by b`,
-			"b", 8000000, "MPP2PhaseAgg"},
-	}
-	return gen4Patterns(ins, ps, n)
+		{
+			`select /*+ read_from_storage(tiflash[t]), mpp_tidb_agg() */ count(a) from t where %v`,
+			[]tempitem{{"t", "b", 0, 20000000}},
+			"MPPTiDBAgg1"}, // mpp tidb agg without group-by
+		{
+			`select /*+ read_from_storage(tiflash[t]), mpp_tidb_agg() */ count(a), b from t where %v group by b`,
+			[]tempitem{{"t", "b", 0, 2000000}},
+			"MPPTiDBAgg2"}, // mpp tidb agg with group-by
+		{
+			`select /*+ read_from_storage(tiflash[t]), mpp_1phase_agg() */ count(a), b from t where %v group by b`,
+			[]tempitem{{"t", "b", 0, 8000000}},
+			"MPP1PhaseAgg"},
+		{
+			`select /*+ read_from_storage(tiflash[t]), mpp_2phase_agg() */ count(a), b from t where %v group by b`,
+			[]tempitem{{"t", "b", 0, 8000000}},
+			"MPP2PhaseAgg"},
+	}, n)
 }
 
-func genSYNJoin(ins utils.Instance, n int) utils.Queries {
-	ps := []pattern{
+func genSYNJoin(n int) utils.Queries {
+	return gen4Templates([]template{
 		// TiKV Join
-		{`select /*+ use_index(t1, b), use_index(t2, b), tidb_hj(t1, t2), read_from_storage(tikv[t1, t2]) */ t1.b, t2.b from t t1, t t2 where t1.b=t2.b and %v`,
-			"b", 2500000, "HashJoin"},
-		{`select /*+ use_index(t1, b), use_index(t2, b), tidb_smj(t1, t2), read_from_storage(tikv[t1, t2]) */ t1.b, t2.b from t t1, t t2 where t1.b=t2.b and %v`,
-			"b", 8000000, "MergeJoin"},
-		{`select /*+ TIDB_INLJ(t1, t2) */ t2.b from t t1, t t2 where t1.b = t2.b and %v`,
-			"b", 500000, "IndexJoin"},
+		{
+			`select /*+ use_index(t1, b), use_index(t2, b), tidb_hj(t1, t2), read_from_storage(tikv[t1, t2]) */ t1.b, t2.b from t t1, t t2 where t1.b=t2.b and %v`,
+			[]tempitem{{"t", "b", 0, 2500000}},
+			"HashJoin"},
+		{
+			`select /*+ use_index(t1, b), use_index(t2, b), tidb_smj(t1, t2), read_from_storage(tikv[t1, t2]) */ t1.b, t2.b from t t1, t t2 where t1.b=t2.b and %v`,
+			[]tempitem{{"t", "b", 0, 8000000}},
+			"MergeJoin"},
+		{
+			`select /*+ TIDB_INLJ(t1, t2) */ t2.b from t t1, t t2 where t1.b = t2.b and %v`,
+			[]tempitem{{"t", "b", 0, 500000}},
+			"IndexJoin"},
 
 		// MPP Join
-		{`SELECT /*+ read_from_storage(tiflash[t1, t2]) */ t1.b, t2.b FROM t t1, t t2 WHERE t1.b=t2.b and %v`,
-			"b", 5000000, "MPPHJ"},
+		{
+			`SELECT /*+ read_from_storage(tiflash[t1, t2]) */ t1.b, t2.b FROM t t1, t t2 WHERE t1.b=t2.b and %v`,
+			[]tempitem{{"t", "b", 0, 5000000}},
+			"MPPHJ"},
 		//{`SELECT /*+ broadcast_join(t1, t2), read_from_storage(tiflash[t1, t2]) */ t1.b, t2.b FROM t t1, t t2 WHERE t1.b=t2.b and %v`,
 		//	"b", 1000, "MPPBCJ"},
-	}
-	return gen4Patterns(ins, ps, n)
+	}, n)
 }
--- costeval/utils.go
+++ costeval/utils.go
@@ -7,44 +7,6 @@ import (
 	"github.com/qw4990/tidb-cost-calibrator/utils"
 )
 
-type pattern struct {
-	sql      string
-	rangeCol string
-	rangeMax int
-	label    string
-}
-
-func gen4Patterns(ins utils.Instance, ps []pattern, n int) utils.Queries {
-	var qs utils.Queries
-	for _, p := range ps {
-		qs = append(qs, gen4Pattern(ins, p, n)...)
-	}
-	return qs
-}
-
-func gen4Pattern(ins utils.Instance, p pattern, n int) utils.Queries {
-	var qs utils.Queries
-	minV, maxV := getColRange4Pattern(ins, p)
-	for i := 0; i < n; i++ {
-		l, r := randRange(minV, maxV, i, n)
-
-		// handle joins specially
-		cond := fmt.Sprintf("%v>=%v and %v<=%v", p.rangeCol, l, p.rangeCol, r)
-		if strings.Contains(p.label, "Join") ||
-			strings.Contains(p.label, "HJ") ||
-			strings.Contains(p.label, "BCJ") {
-			cond = fmt.Sprintf("t1.%v>=%v and t1.%v<=%v and t2.%v>=%v and t2.%v<=%v",
-				p.rangeCol, l, p.rangeCol, r, p.rangeCol, l, p.rangeCol, r)
-		}
-
-		qs = append(qs, utils.Query{
-			SQL:   fmt.Sprintf(p.sql, cond),
-			Label: p.label,
-		})
-	}
-	return qs
-}
-
 type tempitem struct {
 	table  string
 	column string
@@ -82,14 +44,6 @@ func gen4Template(t template, n int) utils.Queries {
 		})
 	}
 	return qs
-}
-
-func getColRange4Pattern(ins utils.Instance, p pattern) (l, r int) {
-	utils.MustReadOneLine(ins, fmt.Sprintf(`select min(%v), max(%v) from %v`, p.rangeCol, p.rangeCol, "t"), &l, &r)
-	if r-l > p.rangeMax {
-		r = l + p.rangeMax
-	}
-	return
 }
 
 func randRange(minVal, maxVal, iter, totalRepeat int) (int, int) {
