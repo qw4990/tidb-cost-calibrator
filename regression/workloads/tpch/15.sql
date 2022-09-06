3c4e2bba84d8f2bec0e6c177eb15876396d2da48
qw4990 <zhangyuanjia@pingcap.com>
2022-09-06 16:17:43 +0800

    fixup

--- regression/workloads/tpch/15.sql
+++ regression/workloads/tpch/15.sql
@@ -1,34 +1,34 @@
 create view revenue0 (supplier_no, total_revenue) as
-	select
-		l_suppkey,
-		sum(l_extendedprice * (1 - l_discount))
-	from
-		lineitem
-	where
-		l_shipdate >= '1997-07-01'
-		and l_shipdate < date_add('1997-07-01', interval '3' month)
-	group by
-		l_suppkey;
+    select
+        l_suppkey,
+        sum(l_extendedprice * (1 - l_discount))
+    from
+        lineitem
+    where
+        l_shipdate >= '1997-07-01'
+        and l_shipdate < date_add('1997-07-01', interval '3' month)
+    group by
+        l_suppkey;
 
 
 select
-	s_suppkey,
-	s_name,
-	s_address,
-	s_phone,
-	total_revenue
+    s_suppkey,
+    s_name,
+    s_address,
+    s_phone,
+    total_revenue
 from
-	supplier,
-	revenue0
+    supplier,
+    revenue0
 where
-	s_suppkey = supplier_no
-	and total_revenue = (
-		select
-			max(total_revenue)
-		from
-			revenue0
-	)
+    s_suppkey = supplier_no
+    and total_revenue = (
+        select
+            max(total_revenue)
+        from
+            revenue0
+    )
 order by
-	s_suppkey;
+    s_suppkey;
 
 drop view revenue0;
