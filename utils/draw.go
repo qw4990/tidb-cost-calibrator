afaaf422d1d2f5a63f116986b3127a9937aaf690
qw4990 <zhangyuanjia@pingcap.com>
2022-09-09 11:07:02 +0800

    fixup

--- utils/draw.go
+++ utils/draw.go
@@ -38,10 +38,12 @@ func DrawCostRecordsTo(r Records, f string) {
 		maxX = math.Max(maxX, x)
 		maxY = math.Max(maxY, y)
 	}
-	p.X.Min = 0
-	p.Y.Min = 0
+	p.X.Min = 1
+	p.Y.Min = 1
 	p.X.Max = maxX * 1.5
 	p.Y.Max = maxY * 1.2
+	p.X.Scale = plot.LogScale{}
+	p.Y.Scale = plot.LogScale{}
 
 	labledRecords := make(map[string]Records)
 	for _, record := range r {
