deeffb7a816d4381440ed3269d993fbd9d978429
qw4990 <zhangyuanjia@pingcap.com>
2022-10-25 18:30:35 +0800

    fixup

--- costeval/regression.go
+++ costeval/regression.go
@@ -45,20 +45,24 @@ func CostRegression() {
 	dataDir := "./data"
 	recordFile := filepath.Join(dataDir, "tpch_clustered-2-true-records.json")
 	utils.Must(utils.ReadFrom(recordFile, &rs))
-	rs = filterByLabel(rs, []string{"TableScan", "HashAgg", "TiDBSel"})
+	rs = filterByLabel(rs, []string{"MPPScan", "TiFlashSel", "1PhaseAgg"})
 	//rs = scaleByLabel(rs, map[string]int{"PhaseAgg": 2})
 
 	fmt.Println("============== shrink factors ===============")
 	rs = shrinkFactors(rs, map[string]float64{
 		"tidb_request_factor": 0,
-		"tidb_cpu_factor":     1,
-		"tikv_cpu_factor":     1,
+		"tiflash_mem_factor":  0,
+		"tidb_cpu_factor":     0,
+		"tidb_kv_net_factor":  0,
+		"tidb_mem_factor":     0,
+		//"tidb_cpu_factor":     1,
+		//"tikv_cpu_factor":     1,
 	})
-	rs = shrinkFactors(rs, map[string]float64{
-		"tidb_kv_net_factor": 1,
-		"tidb_mem_factor":    0.05,
-		"tikv_mem_factor":    0.05,
-	})
+	//rs = shrinkFactors(rs, map[string]float64{
+	//	"tidb_kv_net_factor": 1,
+	//	"tidb_mem_factor":    0.05,
+	//	"tikv_mem_factor":    0.05,
+	//})
 
 	fmt.Println("============== training ===============")
 	x, y, idx2Name := prepareData(rs)
