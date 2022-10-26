25351e70fa8c9a9cebcbd80a598d5526f168c211
qw4990 <zhangyuanjia@pingcap.com>
2022-10-26 14:13:38 +0800

    fixup

--- costeval/calibrate.go
+++ costeval/calibrate.go
@@ -10,7 +10,7 @@ import (
 func CostCalibrate() {
 	var rs utils.Records
 	dataDir := "./data"
-	recordFile := filepath.Join(dataDir, "synthetic-2-true-records.json")
+	recordFile := filepath.Join(dataDir, "tpch_clustered-2-true-records.json")
 	utils.Must(utils.ReadFrom(recordFile, &rs))
 	whiteList := []string{
 		"",
